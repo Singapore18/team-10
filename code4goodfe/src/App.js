5ea3e34c458cbee87808fd4daf11f0c6157dc27f
Le Thanh An <tale.2016@sis.smu.edu.sg>
2018-10-12 14:22:48 +0800

    coach home page

--- code4goodfe/src/App.js
+++ code4goodfe/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 // Import Screens
 import FormPage from './app/form/FormPage';
 import CoachPage from './app/coach/CoachPage';
+import CoachHomePage from './app/coach/CoachHomePage';
 import ResumeContainer from './app/resume/ResumeContainer';
 import ResumeHome from './app/resume/ResumeHome';
 
@@ -25,6 +26,7 @@ class App extends Component {
               <Route exact path='/' component={FormPage}/>
               <Route exact path='/resume/:identifier' component={ResumeContainer}/>
               <Route exact path='/home' component={ResumeHome} />
+              <Route exact path='/coachHome' component={CoachHomePage}/>
               <Route exact path='/coach/:identifier' component={CoachPage}/>
           </Switch>
         </Route>
--- code4goodfe/src/app/coach/CoachPage.js
+++ code4goodfe/src/app/coach/CoachPage.js
@@ -1,17 +1,116 @@
 import React, { Component } from 'react';
+import { Input, Popup, Button } from 'semantic-ui-react'
+import Calendar from 'react-calendar';
+
+import '../assets/css/theme.css';
+import '../assets/css/coach.css';
 
 export default class CoachPage extends Component {
     constructor(props) {
         super(props);
+        this.state={
+            
+        }
     }
+
 
     componentDidMount = async () => {
     }
 
     render() {
         return(
-            <div>
-                <h1>Let us know more about you!</h1>
+            <div class='coach-wrapper'>
+                <h1>Edit your response here:</h1>
+                <div class='coach-questions'>
+                    <div class="column">
+                        <div class='card card-style'>
+                            <h2>What is your NRIC? :</h2>
+                            <Input fluid placeholder='{Type your answer here...}'/>
+                            <br/>
+                            <br/>
+                        </div>
+                    </div>
+                        <div class="column">
+                        <div class='card card-style'>
+                            <h2>What is your full name?</h2>
+                            <Input fluid placeholder='Type your answer here...'/>
+                            <br/>
+                            <br/>
+                        </div>
+                    </div>
+                    <div class='card card-style'>
+                        <h2>What do you like to do in your freetime :D</h2>
+                        <Input fluid placeholder='Type your answer here...'/>
+                        <br/>
+                        <br/>
+                    </div>
+                    <div class='card card-style'>
+                        <h2>What are your strengths?</h2>
+                        <Input fluid placeholder='Type your answer here...'/>
+                        <br/>
+                        <br/>
+                    </div>
+                    <div class='card card-style'>
+                        <h2>What have you worked as before</h2>
+                        <Input fluid placeholder='Type your answer here...'/>
+                        <br/>
+                        <br/>
+                    </div>
+                    <div class='card card-style'>
+                        <h2>What skill do you have?</h2>
+                        <Input fluid placeholder='Type your answer here...'/>
+                        <br/>
+                        <br/>
+                    </div>
+                    <div class='card card-style'>
+                        <h2>What kind of job do you like?</h2>
+                        <Input fluid placeholder='Type your answer here...'/>
+                        <br/>
+                        <br/>
+                    </div>
+                    <div class='card card-style'>
+                        <h2>Please support me by...</h2>
+                        <Input fluid placeholder='Type your answer here...'/>
+                        <br/>
+                        <br/>
+                    </div>
+                    <div class='card card-style'>
+                        <h2>I live near...</h2>
+                        <Input fluid placeholder='Type your answer here...'/>
+                        <br/>
+                        <br/>
+                    </div>
+                    <div class='card card-style'>
+                        <h2>I prefer working in...</h2>
+                        <Input fluid placeholder='Type your answer here...'/>
+                        <br/>
+                        <br/>
+                    </div>
+                    <div class='card card-style'>
+                        <h2>I can work from...</h2>
+                        <Input fluid placeholder='Type your answer here...'/>
+                        <br/>
+                        <br/>
+                    </div>
+                    <div class='card card-style'>
+                        <h2>I can work...</h2>
+                        <Input fluid placeholder='Type your answer here...'/>
+                        <br/>
+                        <br/>
+                    </div>
+                    <div class='card card-style'>
+                        <h2>I can work on...</h2>
+                        <Input fluid placeholder='Type your answer here...'/>
+                        <br/>
+                        <br/>
+                    </div>
+                    <button class='ui inverted button'>
+                        <h4>Save</h4>
+                    </button>
+                    <button class='ui inverted button'>
+                        <h4>Publish </h4>
+                    </button>
+                </div>
             </div>
         )
     }
