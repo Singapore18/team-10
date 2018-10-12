727be18a0fb3f59c231a27f47f338183fe8b974d
Eric Xu <xujinzheng0610@gmail.com>
2018-10-12 11:52:22 +0800

    form_request

--- MINDS_backend/minds_backend/routes/Form.py
+++ MINDS_backend/minds_backend/routes/Form.py
@@ -1,10 +1,36 @@
 from minds_backend import app, mongo
 from flask import session, request, jsonify
 from functools import wraps
+import json
+from bson import json_util
 
 
-@app.route('/form.html', methods=['POST'])
+@app.route('/form', methods=['POST'])
 def store_new_form():
     data = request.get_json()
-    result = 0
+    
+    mongo.forms.insert_one({
+        'NRIC': data['NRIC'],
+        'first_name': data['first_name'],
+        'full_name': data['full_name'],
+        'date': data['date'],
+        'personal_interests': data['personal_interests'],
+        'work_strengths': data['work_strengths'],
+        'work_experience': data['work_experience'],
+        'skills': data['skills'],
+        'support': data['support'],
+        'interested_industries': data['interested_industries'],
+        'request_location': data['request_location'],
+        'request_area': data['request_area'],
+        'request_time': data['request_time'],
+        'request_duration': data['request_duration'],
+        'request_dow': data['request_dow'],
+        'quote': data['quote'],
+        'status': 'pending' # pending, approved, employed
+    })
+
+    result = list(mongo.forms.find())
+    result = json.dumps(result, default=json_util.default)
     return jsonify(result)
+    
+
