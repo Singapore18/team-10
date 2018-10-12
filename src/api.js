660f07ac0f8bfd274df07e62499620fee3f600f7
Le Thanh An <tale.2016@sis.smu.edu.sg>
2018-10-12 10:20:54 +0800

    init

---
+++ src/api.js
@@ -0,0 +1,12 @@
+// #########################################################################
+// ########################### API LISTING #################################
+// #########################################################################
+
+// This file is the listing for the apis that we will be using for the 
+// project. Any changes made to any end points should be update in this file
+
+//export const API_SERVER_URL = '' // URL localhost
+export const API_SERVER_URL = ''; // URL deployed staging
+// export const API_SERVER_URL = '' // URL deployed production
+
+export const LOG_IN = '/api/login/';
---
+++ src/app/form/FormPage.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class CampaignsScreen extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount = async () => {
+    }
+
+    render() {
+        return(
+            <div>
+                <h1>Hello World</h1>
+            </div>
+        )
+    }
+}
