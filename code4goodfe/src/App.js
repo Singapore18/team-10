ebeee956e7d09134af7923248fe93b7b2c0b6726
Ting Hao Ng <tinghao94@gmail.com>
2018-10-12 14:36:14 +0800

    prep for integration

--- code4goodfe/src/App.js
+++ code4goodfe/src/App.js
@@ -6,6 +6,7 @@ import FormPage from './app/form/FormPage';
 import CoachPage from './app/coach/CoachPage';
 import ResumeContainer from './app/resume/ResumeContainer';
 import ResumeHome from './app/resume/ResumeHome';
+// import HirePage from './app/hire/HirePage';
 
 // Import css
 import './app/assets/css/theme.css';
@@ -26,6 +27,7 @@ class App extends Component {
               <Route exact path='/resume/:identifier' component={ResumeContainer}/>
               <Route exact path='/home' component={ResumeHome} />
               <Route exact path='/coach/:identifier' component={CoachPage}/>
+              {/* <Route exact path='/hire/:identifier' component={HirePage} /> */}
           </Switch>
         </Route>
       </Router>
--- code4goodfe/src/app/resume/ResumeContainer.js
+++ code4goodfe/src/app/resume/ResumeContainer.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import ResumePage from './ResumePage';
 import html2canvas from 'html2canvas';
+import {
+    Breadcrumb
+} from 'semantic-ui-react';
 const jsPDF = require('jspdf');
 
 export default class ResumeContainer extends Component {
         state = {
+            id: '',
             fetched: false,
             data: {}
         }
@@ -12,7 +16,15 @@ export default class ResumeContainer extends Component {
     componentDidMount = async () => {
         if (!this.state.fetched) {
             // fetch data here
-
+            const id = this.props.location.pathname.replace('/resume/', '');
+            const ROOT = 'localhost:5000/';
+            fetch(`${ROOT}home/${id}`)
+            .then((res) => {
+                console.log(res);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
             // fake data
             let data = {
                 quote: 'See my capabilities, not my disability!',
@@ -22,9 +34,9 @@ export default class ResumeContainer extends Component {
                     { job_title: 'Dish Washer', company: 'MacDonalds', periodStart: 'Jan 2018', periodEnd: 'May 2018'},
                     {job_title: 'Sales Assistant', company: 'Uniqlo', periodStart: 'Apr 2017', periodEnd: 'Dec 2017'}
                 ],
-                job_interests: ['F&B', 'Hotel'],
-                support_me: 'Please give me simple and clear instructions, encouragement and guidance. I may be slow in the beginning but with time, let me show you my capabilities!',
-                first_name: 'Jason',
+                interested_industries: ['F&B', 'Hotel'],
+                support: 'Please give me simple and clear instructions, encouragement and guidance. I may be slow in the beginning but with time, let me show you my capabilities!',
+                full_name: 'Jason Tan',
                 last_name: 'Tan',
                 request_location: 'Bishan MRT',
                 request_area: 'North East',
@@ -35,6 +47,7 @@ export default class ResumeContainer extends Component {
             // update state
             setTimeout(() => {
                 this.setState({
+                    id,
                     data,
                     fetched: true
                 });
@@ -42,45 +55,61 @@ export default class ResumeContainer extends Component {
         }
         // setTimeout(() => {
         //     this.printDocument()
-        // }, 2000)
+        // }, 1000)
     }
 
-    // printDocument() {
-    //     const input = document.getElementById('divToPrint');
-    //
-    //     var useWidth = input.style.width;
-    //     var useHeight = input.style.height;
-    //
-    //     html2canvas(input, {
-    //         height: useHeight,
-    //         width: useWidth
-    //     })
-    //     .then((canvas) => {
-    //         document.body.appendChild(canvas);
-    //         const imgData = canvas.toDataURL('image/jpeg')
-    //
-    //         const pdf = new jsPDF();
-    //         pdf.addImage(imgData, 'JPEG', 0, 0);
-    //         pdf.save("download.pdf");
-    //     });
-    // }
+    printDocument() {
+        const input = document.getElementById('divToPrint');
+
+        var useWidth = input.style.width;
+        var useHeight = input.style.height;
+
+        html2canvas(input, {
+            height: useHeight,
+            width: useWidth
+        })
+        .then((canvas) => {
+            document.body.appendChild(canvas);
+            const imgData = canvas.toDataURL('image/jpeg')
+
+            const pdf = new jsPDF();
+            pdf.addImage(imgData, 'JPEG', 0, 0);
+            pdf.save("download.pdf");
+        });
+    }
+
+    onHireButtonPress() {
+        this.props.history.push(`/hire/${this.state.id}`);
+    }
+
+    onHomeClick() {
+        this.props.history.push('/home');
+    }
 
     render() {
+        console.log(this.props);
         if (this.state.fetched) {
             return (
                 <div>
-                    <div style={{ height: 50}}>
-                        <text> abc </text>
+                    <div style={{ height: 50, paddingLeft: 120, paddingTop: 15 }}>
+                        <Breadcrumb>
+                            <Breadcrumb.Section
+                                onClick={this.onHomeClick.bind(this)}
+                                link
+                                >
+                                    Home
+                                </Breadcrumb.Section>
+                            <Breadcrumb.Divider />
+                            <Breadcrumb.Section active>Resume</Breadcrumb.Section>
+                        </Breadcrumb>
                     </div>
-                    <ResumePage userInfo={this.state.data}/>
+                    <ResumePage
+                        onHireButtonPress={this.onHireButtonPress.bind(this)}
+                        userInfo={this.state.data}
+                    />
                 </div>
             );
         }
-        return (
-            <div>
-                <h1>Hello World</h1>
-            </div>
-        );
-
+        return <div />
     }
 }
