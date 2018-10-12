00e24ba5711f0c13272556029f1ed3c2bb661106
Anqi Tu <anqitu@outlook.com>
2018-10-12 13:59:37 +0800

    Add job

--- MINDS_backend/minds_backend/__init__.py
+++ MINDS_backend/minds_backend/__init__.py
@@ -11,5 +11,6 @@ app.config['SECRET_KEY'] = '123456'
 
 import minds_backend.routes.Form
 import minds_backend.routes.Offer
+import minds_backend.routes.Job
 #import minds_backend.routes.Article
 #from minds_backend.routes.User import is_logged_in
--- MINDS_backend/minds_backend/routes/Job.py
+++ MINDS_backend/minds_backend/routes/Job.py
@@ -7,30 +7,56 @@ from bson.objectid import ObjectId
 
 
 @app.route('/job', methods=['POST'])
-def store_new_offer():
-    data = request.get_json()
+def store_new_job():
 
-    mongo.offers.insert_one({
-        'employee_id': data['employee_id'],
-        'date': data['date'],
-        'company_name': data['company_name'],
-        'work_location': data['work_location'],
-        'job_title': data['job_title'],
-        'job_description': data['job_description'],
-        'contact_person': data['contact_person'],
-        'contact_number': data['contact_number'],
-        'email_address': data['email_address'],
-        'remarks': data['remarks'],
+    datas = [{
+            'date': '20181001',
+            'company_name': 'Uniqlo',
+            'work_location': 'Jurong East',
+            'job_title': 'Retail Assistant',
+            'job_description': 'Unpack clothes from boxes, fold, sort & hang clothes. Prefer physically fit males.',
+            'contact_person': 'Masaki Ta',
+            'contact_number': '86122242',
+            'email_address': 'goto@uniqlo.com',
+            'industry': 'F&B'
+            },
+            {
+            'date': '20181001',
+            'company_name': 'Subway',
+            'work_location': 'Jurong East',
+            'job_title': 'Cleaner',
+            'job_description': 'Clean floor.',
+            'contact_person': 'Ann Tu',
+            'contact_number': '86789134',
+            'email_address': 'goto@subway.com',
+            'industry': 'Cleaning'
+            },
+            {
+            'date': '20181001',
+            'company_name': 'Changi Hotel',
+            'work_location': 'Changi',
+            'job_title': 'Retail Assistant',
+            'job_description': 'Clean room.',
+            'contact_person': 'Clare Ma',
+            'contact_number': '89612344',
+            'email_address': 'goto@uniqlo.com',
+            'industry': 'Hotel'
+            }]
 
-        'status': 'offered', #offered, accepted,
-    })
 
-    offer_id = list(mongo.offers.find())[-1]['_id']
+    for data in datas:
+        print(data)
+        mongo.jobs.insert_one({
+            'date': data['date'],
+            'company_name': data['company_name'],
+            'work_location': data['work_location'],
+            'job_title': data['job_title'],
+            'job_description': data['job_description'],
+            'contact_person': data['contact_person'],
+            'contact_number': data['contact_number'],
+            'email_address': data['email_address'],
 
-    employee = mongo.forms.find_one({'_id': ObjectId(data['employee_id'])})
-
-    job_offers_current = employee['job_offers']
-    job_offers_current.append(offer_id)
-    mongo.forms.update({'_id':ObjectId(employee['_id'])}, {"$set": {'job_offers': job_offers_current}}, upsert=False)
+            'status': 'open', #closed
+        })
 
     return "Saved"
